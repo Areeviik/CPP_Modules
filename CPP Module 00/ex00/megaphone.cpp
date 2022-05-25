719dca966a5abe72b52f811c61d8467e08b694f1
Areeviik <arevik.xachaturyan@gmail.com>
2022-05-25 15:16:13 +0400

    done 00

---
+++ CPP Module 00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void    to_upper_str(char *s)
+{
+    int i = 0;
+    while(s[i] != '\0')
+    {
+        if (s[i] >= 97 && s[i] <= 122)
+        {
+            s[i] -= 32;
+        }
+        std::cout<<s[i];
+        i++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+    }
+    else
+    {
+        int i = 1;
+        while(i != argc)
+        {
+            to_upper_str(argv[i]);
+            i++;
+        }
+    }
+    return 0;
+}
