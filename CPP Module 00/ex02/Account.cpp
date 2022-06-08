1f27f4674a151fea89b2725c1150263b98fa4fb2
Arevhat Khachaturyan <akhachat@c2r11s4.42yerevan.am>
2022-06-08 15:03:05 +0400

    almost done

--- CPP Module 00/ex02/Account.cpp
+++ CPP Module 00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: akhachat <akhachat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 19:36:15 by akhachat          #+#    #+#             */
-/*   Updated: 2022/06/08 14:40:37 by akhachat         ###   ########.fr       */
+/*   Updated: 2022/06/08 14:56:06 by akhachat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,34 @@ void Account::displayAccountsInfos(void)
 
 Account::Account(int initial_deposit)
 {
-
+    std::cout<<"index:"<<Account::_accountIndex<<";";
+    std::cout<<"amount:"<<Account::_amount<<";";
+    std::cout<<"created"<<std::endl;
 }
 
 Account::~Account(void)
 {
-    
+    std::cout<<"index:"<<Account::_accountIndex<<";";
+    std::cout<<"amount:"<<Account::_amount<<";";
+    std::cout<<"closed"<<std::endl;
 }
 
 void Account::makeDeposit(int deposit)
 {
-    
+    std::cout<<"index:"<<Account::_accountIndex<<";";
+    std::cout<<"p_amount:"<<Account::_amount<<";";
+    std::cout<<"deposit:"<<deposit<<";";
+    std::cout<<"amount:"<<Account::_totalAmount<<";";
+    std::cout<<"nb_deposits:"<<Account::_nbDeposits<<std::endl;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
-    
+    std::cout<<"index:"<<Account::_accountIndex<<";";
+    std::cout<<"p_amount:"<<Account::_amount<<";";
+    std::cout<<"withdrawal:"<<withdrawal<<";";
+    std::cout<<"amount:"<<Account::_totalAmount<<";";
+    std::cout<<"nb_withdrawals:"<<Account::_nbWithdrawals<<std::endl;
 }
 
 int Account::checkAmount(void) const
@@ -70,5 +82,6 @@ void Account::displayStatus(void) const
 {
     std::cout<<"index:"<<Account::_accountIndex<<";";
     std::cout<<"amount:"<<Account::_amount<<";";
+    std::cout<<"deposits:"<<Account::_nbDeposits<<";";
     std::cout<<"withdrawal:"<<Account::_nbDeposits<<std::endl;
 }
