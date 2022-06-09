6210cff74209e67ed2890a6c5091823be1da3ea1
Arevhat Khachaturyan <akhachat@c2r11s4.42yerevan.am>
2022-06-09 15:21:13 +0400

    CPP00

---
+++ CPP Module 01/ex00/Zombie.hpp
@@ -0,0 +1,18 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+    public:
+        Zombie* newZombie(std::string name);
+        void randomChump(std::string name);
+        void announce(void);
+    private:
+        std::string name;
+        Zombie();
+        ~Zombie();
+};
+
+#endif
