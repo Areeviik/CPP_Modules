b2847c765c6cf2736b0835d089ef3da665c8b08a
Arevhat Khachaturyan <akhachat@c2r11s4.42yerevan.am>
2022-06-16 15:22:57 +0400

    06 fixed

--- CPP Module 01/ex06/Karen.cpp
+++ CPP Module 01/ex06/Karen.cpp
@@ -33,18 +33,21 @@ void Karen::debug(void)
 {
     std::cout<<"\033[1;31m[DEBUG]\033[0m"<<std::endl;
     std::cout<<"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+    info();
 }
 
 void Karen::info(void)
 {
     std::cout<<"\033[1;31m[INFO]\033[0m"<<std::endl;
     std::cout<<"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+    warning();
 }
 
 void Karen::warning(void)
 {
     std::cout<<"\033[1;31m[WARNING]\033[0m"<<std::endl;
     std::cout<<"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
+    error();
 }
 
 void Karen::error(void)
--- CPP Module 01/ex06/main.cpp
+++ CPP Module 01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: akhachat <akhachat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 14:50:21 by akhachat          #+#    #+#             */
-/*   Updated: 2022/06/16 15:17:03 by akhachat         ###   ########.fr       */
+/*   Updated: 2022/06/16 15:22:48 by akhachat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,9 @@
 int main(int argc, char **argv)
 {
     Karen obj;
-    std::string lvl[4]={"DEBUG", "INFO", "WARNING", "ERROR"};
-
     if (argc != 2)
         std::cout<<"\033[1;31mPlease enter one of these levels as parameter: DEBUG, INFO, WARNING, ERROR\033[0m"<<std::endl;
-    for (int i = 0; i< 4; i++)
-    {
-        if (lvl[i] == argv[1])
-        {
-            obj.complain(lvl[i]);
-            obj.complain(lvl[++i]);
-        }
-    }
+    else
+        obj.complain(argv[1]);
     return 0;
 }
