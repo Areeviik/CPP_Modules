6521645e76dec1f235d2aafc22b77ec22e25e866
Arevhat Khachaturyan <akhachat@c2r11s4.42yerevan.am>
2022-06-16 15:45:26 +0400

    CPP01 done

--- CPP Module 01/ex04/main.cpp
+++ CPP Module 01/ex04/main.cpp
@@ -14,6 +14,24 @@
 #include <string.h>
 #include <fstream>
 
+std::string ft_replace(std::string read, std::string s1, std::string s2)
+{
+    int i = 0;
+    int pos = 0;
+    std::string rep;
+    while (1)
+    {
+        pos = read.find(s1, i);
+        if (pos == std::string::npos)
+            break;
+        rep.append(read, i, pos - i);
+        i = pos + s1.size();
+        rep.append(s2);
+    }
+    rep.append(read, i);
+    return rep;
+}
+
 int main(int argc, char **argv)
 {
     std::ifstream fn;
@@ -25,30 +43,36 @@ int main(int argc, char **argv)
 
     if (argc != 4)
     {
-        std::cout<<"Wrong number of arguments"<<std::endl;
-        std::cout<<"Program takes three arguments: filename, string1 and string 2"<<std::endl;
-        return -1;
+        std::cout<<"\033[1;31mWrong number of arguments\033[0m"<<std::endl;
+        std::cout<<"\033[1;31mProgram takes three arguments: <filename> <search> <replace>\033[0m"<<std::endl;
+        return 42;
     }
-    fn.open(argv[1]);
-    if (!fn)
-        std::cout<<"Can't open a file"<<std::endl;
+    fn.open(argv[1], std::fstream::out);
+    if (!fn.is_open())
+    {
+        std::cout<<"Can't open input file"<<std::endl;
+        return 42;
+    }
     s1 = argv[2];
     s2 = argv[3];
 
     fcopy = argv[1];
     fcopy += ".replace";
-    if (s1 == s2)
+    replace.open(fcopy, std::fstream::in | std::ifstream::trunc);
+
+    if (!replace.is_open())
     {
-        while (std::getline(fn, read))
-            replace<<read<<std::endl;
-        replace.close();
-        return (0);
-    }
-    while (std::getline(fn, read))
-    {
-        
+        std::cout<<"Can't open output file"<<std::endl;
+        return 42;
     }
 
-
-
+    while (std::getline(fn, read, '\n').good())
+    {
+        replace<<ft_replace(read, s1, s2);
+        if (!fn.eof())
+            replace<<'\n';
+    }
+    fn.close();
+    replace.close();
+    return 0;
 }
