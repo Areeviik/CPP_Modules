d96e418330c3b3be47e883df12835ce5a14c5a6f
Areeviik <arevik.xachaturyan@gmail.com>
2022-07-24 23:05:56 +0400

    07-01 fixed main

--- CPP Module 07/ex01/main.cpp
+++ CPP Module 07/ex01/main.cpp
@@ -1,53 +1,19 @@
 #include <iostream>
-#include <Array.hpp>
+#include "iter.hpp"
+#include <string.h>
+#include <unistd.h>
 
-#define MAX_VAL 750
-int main(int, char**)
+template<typename T>
+void ft_print(T &el)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+    std::cout "~~~~~~~"<<el<<"~~~~~~~~~"<<std::endl;
+}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
+int main()
+{
+    std::string str[4] = {"hi", "how", "are", "you"};
+    int arr[3] = {1,2,3};
+    iter(str, 4, ft_print);
+    iter(arr, 3, ft_print);
     return 0;
 }
