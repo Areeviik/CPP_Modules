9a435acd3a3cc67dafe1e5efddb5118bc6ee4e71
Areeviik <arevik.xachaturyan@gmail.com>
2022-07-28 23:32:16 +0400

    06-02 minor errors

--- CPP Module 06/ex01/main.cpp
+++ CPP Module 06/ex01/main.cpp
@@ -7,8 +7,8 @@ int main()
     uintptr_t u = serialize(d);
     Data *ptr = deserialize(u);
     std::cout<<"d->str:"<<d->str<<std::endl;
-    std::cout<<"Result of serialize function"<<u<<std::endl;
-    std::cout<<"Result of deserialize function"<<ptr->str<<std::endl;
+    std::cout<<"Result of serialize function: "<<u<<std::endl;
+    std::cout<<"Result of deserialize function: "<<ptr->str<<std::endl;
 
     delete d;
     return 0;
