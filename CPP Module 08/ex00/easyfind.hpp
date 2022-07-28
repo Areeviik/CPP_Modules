cd2f5acafbbeeb680905b8e644103f95965f3ef2
Areeviik <arevik.xachaturyan@gmail.com>
2022-07-28 23:57:03 +0400

    08-00 done

--- CPP Module 08/ex00/easyfind.hpp
+++ CPP Module 08/ex00/easyfind.hpp
@@ -8,8 +8,8 @@
 template <typename T>
 typename T::iterator easyfind(T &src, int el)
 {
-    if (!std::find(src.begin(), src.end(), el))
-        return "No occurrence is found";
+    if (!(*std::find(src.begin(), src.end(), el)))
+        throw std::length_error("No occurrence is found");
     return (std::find(src.begin(), src.end(), el));
 }
 
--- CPP Module 08/ex00/main.cpp
+++ CPP Module 08/ex00/main.cpp
@@ -12,5 +12,17 @@ int main()
     l.push_back(23);
 
     std::list<int>::iterator i = easyfind(l, 23);
+    if (i == l.end())
+        std::cout<<"No occurrence is found"<<std::endl;
+    else
+        std::cout<<"Element "<<*i<<" is found"<<std::endl;
+
+    std::list<int>::iterator i2 = easyfind(l, 10000);
+    if (i2 == l.end())
+        std::cout<<"No occurrence is found"<<std::endl;
+    else
+        std::cout<<"Element "<<*i2<<" is found"<<std::endl;
+    
+    
     return 0;
 }
